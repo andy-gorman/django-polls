d0bcd95f6a8c60e8ef3461cfd90af71abd0cee6a
Andrew Gorman <a.c.gorman@wustl.edu>
2014-05-25 19:11:53 -0400

    Added basic urls

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("Hello World. You are at the poll index")
 
+def detail(request, poll_id): 
+	return HttpResponse("You are looking at poll %s" % poll_id)
+
+def results(request, poll_id):
+	return HttpResponse("You are looking at the results of poll %s" % poll_id)
+
+def vote(request, poll_id):
+	return HttpResponse("You are voting for poll %s" % poll_id)
+
+
+
 
