2c7a2e29189a8f7a7164bfbb459643ab2b622bd9
Andrew Gorman <a.c.gorman@wustl.edu>
2014-05-30 12:01:31 -0400

    Finished part 3 of the tutorial, extremely basic templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls')),
+	#namespace argument lets django diffenteriate between the different
+	#apps.
+    url(r'^polls/', include('polls.urls'), namespace="polls"),
     url(r'^admin/', include(admin.site.urls)),
 )
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+
+from polls.models import Poll
 
 def index(request):
-	return HttpResponse("Hello World. You are at the poll index")
+	latest_poll_list = Poll.objects.order_by('pub_date')[:5]
+	context = {'latest_poll_list': latest_poll_list}
+	return render(request, 'polls/index.html', context)
+	
+	"""Below is the same, more or less, as above"""
+	#template = loader.get_template('polls/index.html')
+	#context = RequestContext(request, {
+	#	'latest_poll_list': latest_poll_list
+	#	})
+	#return HttpResponse(template.render(context))
 
 def detail(request, poll_id): 
-	return HttpResponse("You are looking at poll %s" % poll_id)
+	
+	poll = get_object_or_404(Poll, pk=poll_id) 
+	
+	#Above line does the exact same thing as the code below.
+	#try:
+	#	poll = Poll.objects.get(pk=poll_id)
+	#except Poll.DoesNotExist:
+	#	raise Http404
+	
+	return render(request, 'polls/detail.html', {'poll': poll})
+
 
 def results(request, poll_id):
 	return HttpResponse("You are looking at the results of poll %s" % poll_id)
